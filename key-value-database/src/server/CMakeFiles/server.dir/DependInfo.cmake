89344923fe4222bc0d0f063bf7cd48c57d6a54f8
MichaelPapinutto <michaelpapinutto@live.fr>
2018-03-22 02:25:06 +0100

    Added server and client base code

---
+++ key-value-database/src/server/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/papinutt/Unifr/BScInformatique/2eme/SA18/OS/Project/UniFr-OS-Project-SP18/key-value-database/src/server/main.c" "/Users/papinutt/Unifr/BScInformatique/2eme/SA18/OS/Project/UniFr-OS-Project-SP18/key-value-database/src/server/CMakeFiles/server.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

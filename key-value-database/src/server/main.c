a285d5a73b0c68a3d96d6d62932ef5086fd78703
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-03-23 11:54:22 +0100

    set void for arguments of the main method to avoid possible passing argument...
    Add a TODO idea if we have time
    
    Also refactor the source code according to my setting, maybe everybody should have the same settings :)

--- key-value-database/src/server/main.c
+++ key-value-database/src/server/main.c
@@ -9,40 +9,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-#include <time.h> 
+#include <time.h>
 
-int main(int argc, char *argv[])
+int main(void)
 {
     int listenfd = 0, connfd = 0;
-    struct sockaddr_in serv_addr; 
+    struct sockaddr_in serv_addr;
 
     char sendBuff[1025];
-    time_t ticks; 
+    time_t ticks;
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr, '0', sizeof(serv_addr));
-    memset(sendBuff, '0', sizeof(sendBuff)); 
+    memset(sendBuff, '0', sizeof(sendBuff));
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(5000); 
+    serv_addr.sin_port = htons(5000);
 
-    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+    bind(listenfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
 
-    listen(listenfd, 10); 
+    listen(listenfd, 10);
 
-    while(1)
+    while (1)
     {
-        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
+        connfd = accept(listenfd, (struct sockaddr*) NULL, NULL);
 
         ticks = time(NULL);
         snprintf(sendBuff, sizeof(sendBuff), "Server provide you the local time: %.24s\r\n", ctime(&ticks));
-        write(connfd, sendBuff, strlen(sendBuff)); 
+        write(connfd, sendBuff, strlen(sendBuff));
 
         close(connfd);
         sleep(1);
-     }
+        // TODO Idea :
+        /*
+         *  Add a stop command with password etc...
+         *  to stop the server manually if needed
+         */
+    }
 }

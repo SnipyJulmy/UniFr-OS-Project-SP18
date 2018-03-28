bde1b8557233b9306c874721b84d9da1220b5075
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-03-28 14:33:24 +0200

    Hashcode implementation (srly???)

---
+++ key-value-database/src/server/lock_free_set/lock_free_set_hash.c
@@ -0,0 +1,11 @@
+//
+// Created by snipy on 16.03.18.
+//
+
+#include "lock_free_set_hash.h"
+
+uint32_t lock_free_set_hash(void* s)
+{
+    // ok...
+    return (uint32_t) s;
+}
---
+++ key-value-database/src/server/lock_free_set/lock_free_set_hash.h
@@ -0,0 +1,13 @@
+//
+// Created by snipy on 16.03.18.
+//
+
+#ifndef LOCK_FREE_SET_LOCK_FREE_SET_HASH_H
+#define LOCK_FREE_SET_LOCK_FREE_SET_HASH_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+uint32_t lock_free_set_hash(void* data);
+
+#endif //LOCK_FREE_SET_LOCK_FREE_SET_HASH_H

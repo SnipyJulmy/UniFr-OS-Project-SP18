ed5ded2b44dc1d3305b77c79bd48566adbaf93b8
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-03-28 09:49:11 +0200

    Add some usefull debug macro...

---
+++ key-value-database/src/debug.h
@@ -0,0 +1,67 @@
+//
+// Created by snipy on 27.03.18.
+//
+
+#ifndef KEY_VALUE_DATABASE_DEBUG_H
+#define KEY_VALUE_DATABASE_DEBUG_H
+
+/*
+ * Configuration
+ */
+
+#define NDEBUG
+#define LOG_LEVEL LOG_LEVEL_DEBUG
+
+/*
+ * Definition
+ */
+
+// -- Log level
+#define LOG_LEVEL_DEBUG 4
+#define LOG_LEVEL_INFO  3
+#define LOG_LEVEL_WARN  2
+#define LOG_LEVEL_ERR   1
+#define LOG_LEVEL_NONE  0
+
+// -- Color
+#define RED    "\e[31m"
+#define GREEN  "\e[32m"
+#define YELLOW "\e[33m"
+#define WHITE  "\e[1m"
+
+// reset color
+#define COLOR_X "\e[m"
+
+// -- Utils
+#define clean_errno() (errno == 0 ? "None" : strerror(errno))
+
+// -- Logging
+#if !defined(NDEBUG) && (LOG_LEVEL >= LOG_LEVEL_ERR)
+    #define log_err(M, ...) fprintf(stderr, "[ERR] (%s:%d: errno: %s): " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#else
+    #define log_err(M, ...)
+#endif
+
+#if !defined(NDEBUG) && (LOG_LEVEL >= LOG_LEVEL_WARN)
+    #define log_warn(M, ...) fprintf(stderr, "[WAR] (%s:%d: errno: %s): " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#else
+    #define log_warn(M, ...)
+#endif
+
+#if !defined(NDEBUG) && (LOG_LEVEL >= LOG_LEVEL_INFO)
+#define log_info(M, ...)      fprintf(stderr, "[INF]: " M "\n", ##__VA_ARGS__)
+    #define log_info_full(M, ...) fprintf(stderr, "[INF] (%s:%d): " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#else
+    #define log_info(M, ...)
+    #define log_info_full(M, ...)
+#endif
+
+#if !defined(NDEBUG) && (LOG_LEVEL >= LOG_LEVEL_DEBUG)
+#define debug(M, ...)      fprintf(stderr, "[DBG]: " M "\n", ##__VA_ARGS__)
+    #define debug_full(M, ...) fprintf(stderr, "[DBG] %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#else
+    #define debug(M, ...)
+    #define debug_full(M, ...)
+#endif
+
+#endif //KEY_VALUE_DATABASE_DEBUG_H

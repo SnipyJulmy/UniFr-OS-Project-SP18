d31789b4acb5b13d0463602b32c2413a106bfd5b
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-03-28 14:27:55 +0200

    Add header and declaration for the database actions

---
+++ key-value-database/src/server/database_actions.c
@@ -0,0 +1,41 @@
+//
+// Created by snipy on 28.03.18.
+//
+
+#include <stdlib.h>
+#include "database_actions.h"
+
+uint32_t database_actions_insert_v(void* value)
+{
+    return 0;
+}
+
+bool database_actions_insert_kv(uint32_t key, void* value)
+{
+    return 0;
+}
+
+void* database_actions_read_k(uint32_t key)
+{
+    return NULL;
+}
+
+bool database_actions_contains_k(uint32_t key)
+{
+    return 0;
+}
+
+bool database_actions_contains_v(void* value)
+{
+    return 0;
+}
+
+bool database_actions_contains_kv(uint32_t key, void* value)
+{
+    return 0;
+}
+
+void database_actions_init(void)
+{
+
+}
---
+++ key-value-database/src/server/database_actions.h
@@ -0,0 +1,32 @@
+//
+// Created by snipy on 28.03.18.
+//
+
+/*
+ *  List of available command to send to the server
+ *  "$>" indicates the terminal like interface
+ *  ">>>" indicates the value return by the server
+ *
+ *  $> insert <value> >>> <key>
+ *  $> insert <key> <value> >>> <bool>
+ *  $> read <key> >>> <value>
+ *  $> contains <key> >>> <bool>
+ *  $> contains <value> >>> <bool>
+ *  $> contains <key> <value> >>> <bool>
+ */
+
+#ifndef KEY_VALUE_DATABASE_DATABASE_ACTIONS_H
+#define KEY_VALUE_DATABASE_DATABASE_ACTIONS_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+uint32_t database_actions_insert_v(void* value);
+bool database_actions_insert_kv(uint32_t key, void* value);
+void* database_actions_read_k(uint32_t key);
+bool database_actions_contains_k(uint32_t key);
+bool database_actions_contains_v(void* value);
+bool database_actions_contains_kv(uint32_t key, void* value);
+void database_actions_init(void);
+
+#endif //KEY_VALUE_DATABASE_DATABASE_ACTIONS_H

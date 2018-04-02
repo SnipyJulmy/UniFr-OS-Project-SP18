5f0266487047924b8a7991833bd2c2d46283f157
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-04-02 11:54:52 +0200

    Add type definition for the value and the key stored in the database

---
+++ key-value-database/src/server/resources_allocator/key_value_database_typedef.h
@@ -0,0 +1,13 @@
+//
+// Created by snipy on 02.04.18.
+//
+
+#ifndef KEY_VALUE_DATABASE_KEY_VALUE_DATABASE_TYPEDEF_H
+#define KEY_VALUE_DATABASE_KEY_VALUE_DATABASE_TYPEDEF_H
+
+#include <stdint.h>
+
+typedef uint32_t K;
+typedef char* V;
+
+#endif //KEY_VALUE_DATABASE_KEY_VALUE_DATABASE_TYPEDEF_H

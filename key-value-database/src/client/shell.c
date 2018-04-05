99af90d47cd177725b5345ae4779d0255e04a78e
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-04-05 15:41:37 +0200

    reformat code and defines status for the shell

--- key-value-database/src/client/shell.c
+++ key-value-database/src/client/shell.c
@@ -12,6 +12,9 @@
 #define SHELL_BUFFER_SIZE 1024
 #define SHELL_TOKEN_DELIMITER " \t\n\r\a"
 
+#define STATUS_OK 1
+#define STATUS_EXIT 0
+
 static char* shell_read_line();
 static Command* shell_tokenize_line(char* line);
 static int shell_execute(Command* command);
@@ -32,7 +35,7 @@ void shell_loop()
 
         free(line);
         command->destroy(command);
-    } while (status);
+    } while (status != STATUS_EXIT);
 }
 
 Command* shell_command_create(char** args, int argc)
@@ -54,68 +57,60 @@ static void shell_command_destroy(Command* self)
 static int shell_execute(Command* command)
 {
     printf("Shell execute command : ");
-/*    for (int i = 0; i < command->argc; i++)
+
+    if (strcmp(command->args[0], "ls") == 0)
     {
-*/
-
-            if (strcmp(command->args[0],"ls") == 0)
-            {
-                //Request for list
-                printf("ls\n");
-            }
-            else if (strcmp(command->args[0],"add") == 0)
-            {
-                //Request for list
-                printf("add\n");
-
-            }
-            else if (strcmp(command->args[0],"readkey") == 0)
-            {
-                //Request for list
-                printf("readkey\n");
-
-            }
-            else if (strcmp(command->args[0],"readval") == 0)
-            {
-                //Request for list
-                printf("readval\n");
-
-            }
-            else if (strcmp(command->args[0],"delkey") == 0)
-            {
-                //Request for list
-                printf("delkey\n");
-
-            }
-            else if (strcmp(command->args[0],"delval") == 0)
-            {
-                //Request for list
-                printf("delval\n");
-
-            }
-            else if (strcmp(command->args[0],"chgkey") == 0)
-            {
-                //Request for list
-                printf("chgkey\n");
-
-            }
-            else if (strcmp(command->args[0],"chgval") == 0)
-            {
-                //Request for list
-                printf("chgval\n");
-
-            }
-            else if (strcmp(command->args[0],"q") == 0)
-            {
-                //Request for list
-                printf("q\n");
-            }
-
-
-/*        printf("%s ", command->args[i]);
+        //Request for list
+        printf("ls\n");
     }
-    printf("\n");
-*/
+    else if (strcmp(command->args[0], "add") == 0)
+    {
+        //Request for list
+        printf("add\n");
+
+    }
+    else if (strcmp(command->args[0], "readkey") == 0)
+    {
+        //Request for list
+        printf("readkey\n");
+
+    }
+    else if (strcmp(command->args[0], "readval") == 0)
+    {
+        //Request for list
+        printf("readval\n");
+    }
+    else if (strcmp(command->args[0], "delkey") == 0)
+    {
+        //Request for list
+        printf("delkey\n");
+
+    }
+    else if (strcmp(command->args[0], "delval") == 0)
+    {
+        //Request for list
+        printf("delval\n");
+
+    }
+    else if (strcmp(command->args[0], "chgkey") == 0)
+    {
+        //Request for list
+        printf("chgkey\n");
+
+    }
+    else if (strcmp(command->args[0], "chgval") == 0)
+    {
+        //Request for list
+        printf("chgval\n");
+
+    }
+    else if (strcmp(command->args[0], "q") == 0)
+    {
+        //Request for list
+        printf("q\n");
+    }
+
+    return STATUS_OK;
 }
 
 static Command* shell_tokenize_line(char* line)
@@ -141,7 +136,7 @@ static Command* shell_tokenize_line(char* line)
         token = strtok(NULL, SHELL_TOKEN_DELIMITER);
     }
     tokens[position] = NULL;
-    return shell_command_create(tokens,position);
+    return shell_command_create(tokens, position);
 }
 
 // read a line from the command line interface

0dc024b435b9b5877d433d81ee30576a89198941
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-04-19 14:17:00 +0200

    remove unused statements at the end of the main function

--- src/client/main.c
+++ src/client/main.c
@@ -7,7 +7,6 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <arpa/inet.h>
 #include <errno.h>
 #include <stdlib.h>
@@ -20,20 +19,19 @@
 int main(int argc, char* argv[])
 {
     int sockfd = 0;
-    ssize_t n = 0;
     char recvBuff[1024];
     struct sockaddr_in serv_addr;
 
     if (argc != 2)
     {
-        printf("\n Usage: %s <ip of server> \n", argv[0]);
+        log_err("\n Usage: %s <ip of server> \n", argv[0]);
         return 1;
     }
 
     memset(recvBuff, '0', sizeof(recvBuff));
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("\n Error : Could not create socket \n");
+        log_err("\n Error : Could not create socket \n");
         return 1;
     }
 
@@ -44,8 +42,8 @@ int main(int argc, char* argv[])
 
     if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0)
     {
-        printf("Errno error : %s\n", strerror(errno));
-        printf("\n inet_pton error occured\n");
+        log_err("Errno error : %s\n", strerror(errno));
+        log_err("\n inet_pton error occured\n");
         return 1;
     }
 
@@ -56,24 +54,5 @@ int main(int argc, char* argv[])
     }
 
     shell_loop(sockfd, &serv_addr);
-
-    char sendBuff[BUFFER_LENGTH];
-    snprintf(sendBuff, sizeof(sendBuff), "insert 1 \"AD\"\n");
-    write(sockfd, sendBuff, strlen(sendBuff));
-
-    while ((n = read(sockfd, recvBuff, sizeof(recvBuff) - 1)) > 0)
-    {
-        recvBuff[n] = 0;
-        if (fputs(recvBuff, stdout) == EOF)
-        {
-            log_err("Fupts error : %s\n", strerror(errno));
-        }
-    }
-
-    if (n < 0)
-    {
-        log_err("Read error : %s\n", strerror(errno));
-    }
-
     return EXIT_SUCCESS;
 }

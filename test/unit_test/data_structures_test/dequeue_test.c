9d6ff0b1be7062900d611b994d60f5787a21c1e4
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-04-25 16:16:42 +0200

    add some tests for the dequeue

--- test/unit_test/data_structures_test/dequeue_test.c
+++ test/unit_test/data_structures_test/dequeue_test.c
@@ -81,5 +81,113 @@ CTEST(dequeue, add)
 
 CTEST(dequeue, remove)
 {
+    Dequeue* dequeue = key_value_database_dequeue_create(sizeof(int), int_cmp, int_destroy);
+    check_mem_and_exit(dequeue);
+    VALUES_1_6;
 
+    ADD(1);
+    AT_FIRST(1);
+    ASSERT_SIZE(1);
+    REMOVE(1);
+    ASSERT_SIZE(0);
+    ADD(2);
+    AT_FIRST(2);
+    ASSERT_SIZE(1);
+    ADD(3);
+    AT_FIRST(2);
+    ASSERT_SIZE(2);
+    ADD_FIRST(4);
+    AT_FIRST(4);
+    ASSERT_SIZE(3);
+    ADD_FIRST(5);
+    AT_FIRST(5);
+    ASSERT_SIZE(4);
+    ADD_FIRST(6);
+    AT_FIRST(6);
+    ASSERT_SIZE(5);
+    ADD(1);
+    AT_FIRST(6);
+    ASSERT_SIZE(6);
+    REMOVE(1);
+    AT_FIRST(6);
+    ASSERT_SIZE(5)
+    REMOVE(2);
+    AT_FIRST(6);
+    ASSERT_SIZE(4);
+    REMOVE(3);
+    AT_FIRST(6);
+    ASSERT_SIZE(3);
+    REMOVE(4);
+    AT_FIRST(6);
+    ASSERT_SIZE(2);
+    REMOVE(5);
+    AT_FIRST(6);
+    ASSERT_SIZE(1);
+    REMOVE(6);
+    ASSERT_SIZE(0);
+    ADD_ALL;
+    ASSERT_SIZE(6);
+    ASSERT_TRUE(REMOVE_FIRST_V == 6);
+    ASSERT_SIZE(5);
+    ASSERT_TRUE(REMOVE_FIRST_V == 5);
+    ASSERT_SIZE(4);
+    ASSERT_TRUE(REMOVE_FIRST_V == 4);
+    ASSERT_SIZE(3);
+    ASSERT_TRUE(REMOVE_FIRST_V == 3);
+    ASSERT_SIZE(2);
+    ASSERT_TRUE(REMOVE_FIRST_V == 2);
+    ASSERT_SIZE(1);
+    ASSERT_TRUE(REMOVE_FIRST_V == 1);
+    ASSERT_SIZE(0);
+    ADD_ALL;
+    ASSERT_SIZE(6);
+    ASSERT_TRUE(REMOVE_LAST_V == 1);
+    ASSERT_SIZE(5);
+    ASSERT_TRUE(REMOVE_LAST_V == 2);
+    ASSERT_SIZE(4);
+    ASSERT_TRUE(REMOVE_LAST_V == 3);
+    ASSERT_SIZE(3);
+    ASSERT_TRUE(REMOVE_LAST_V == 4);
+    ASSERT_SIZE(2);
+    ASSERT_TRUE(REMOVE_LAST_V == 5);
+    ASSERT_SIZE(1);
+    ASSERT_TRUE(REMOVE_LAST_V == 6);
+    ASSERT_SIZE(0);
+
+    dequeue->destroy(dequeue);
+}
+
+CTEST(dequeue, contains)
+{
+    Dequeue* dequeue = key_value_database_dequeue_create(sizeof(int), int_cmp, int_destroy);
+    check_mem_and_exit(dequeue);
+    dequeue->destroy(dequeue);
+}
+
+CTEST(dequeue, move_n_first_to_front)
+{
+    Dequeue* dequeue = key_value_database_dequeue_create(sizeof(int), int_cmp, int_destroy);
+    check_mem_and_exit(dequeue);
+    dequeue->destroy(dequeue);
+}
+
+CTEST(dequeue, move_n_last_to_front)
+{
+    Dequeue* dequeue = key_value_database_dequeue_create(sizeof(int), int_cmp, int_destroy);
+    check_mem_and_exit(dequeue);
+    dequeue->destroy(dequeue);
+}
+
+CTEST(dequeue, move_n_first_to_end)
+{
+    Dequeue* dequeue = key_value_database_dequeue_create(sizeof(int), int_cmp, int_destroy);
+    check_mem_and_exit(dequeue);
+    dequeue->destroy(dequeue);
+}
+
+CTEST(dequeue, move_n_last_to_end)
+{
+    Dequeue* dequeue = key_value_database_dequeue_create(sizeof(int), int_cmp, int_destroy);
+    check_mem_and_exit(dequeue);
+    dequeue->destroy(dequeue);
 }
--- test/unit_test/data_structures_test/dequeue_test_macro.h
+++ test/unit_test/data_structures_test/dequeue_test_macro.h
@@ -7,17 +7,36 @@
 
 #define VALUE(ID_INT) int v##ID_INT = ID_INT; int* p##ID_INT = &v##ID_INT;
 
-#define ADD(ID_INT) do {dequeue->add(dequeue,p##ID_INT);} while(0);
-#define ADD_FIRST(ID_INT) do {dequeue->add_first(dequeue,p##ID_INT);} while(0);
-#define PEEK_INT(ID_INT) (*(int*)(dequeue->peek_first(dequeue)))
 #define AT_FIRST_V(ID_INT) ASSERT_TRUE(PEEK_INT(ID_INT) == v##ID_INT);
 #define AT_FIRST_P(ID_INT) ASSERT_TRUE(dequeue->peek_first(dequeue) == p##ID_INT);
 #define AT_FIRST(ID_INT) do {AT_FIRST_P(ID_INT); AT_FIRST_V(ID_INT);}while(0);
+
+#define ADD(ID_INT) do {dequeue->add(dequeue,p##ID_INT);} while(0);
+#define ADD_FIRST(ID_INT) do {dequeue->add_first(dequeue,p##ID_INT);} while(0);
+// add 1,2,3,4,5,6... so the stack should be 6->5->4->3->2->1
+#define ADD_ALL do{\
+    ADD_FIRST(1);AT_FIRST(1);\
+    ADD_FIRST(2);AT_FIRST(2);\
+    ADD_FIRST(3);AT_FIRST(3);\
+    ADD_FIRST(4);AT_FIRST(4);\
+    ADD_FIRST(5);AT_FIRST(5);\
+    ADD_FIRST(6);AT_FIRST(6);\
+    } while(0);
+
+#define REMOVE(ID_INT) do {dequeue->remove(dequeue,p##ID_INT);} while(0);
+#define REMOVE_FIRST_P ((int*)dequeue->remove_first(dequeue))
+#define REMOVE_FIRST_V (*(int*)dequeue->remove_first(dequeue))
+#define REMOVE_LAST_P ((int*)dequeue->remove_last(dequeue))
+#define REMOVE_LAST_V (*(int*)dequeue->remove_last(dequeue))
+
+#define PEEK_INT(ID_INT) (*(int*)(dequeue->peek_first(dequeue)))
 #define ASSERT_SIZE(EXPECTED_SIZE) do{\
     if((EXPECTED_SIZE) == 0)\
         ASSERT_TRUE(dequeue->is_empty(dequeue));\
     else \
         ASSERT_FALSE(dequeue->is_empty(dequeue));\
     ASSERT_TRUE(dequeue->size == (EXPECTED_SIZE));}while(0);
+
+#define VALUES_1_6 VALUE(1);VALUE(2);VALUE(3);VALUE(4);VALUE(5);VALUE(6);
 
 #endif //KEY_VALUE_DATABASE_DEQUEUE_TEST_MACRO_H

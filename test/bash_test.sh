1c36e9861bbbacc6a40223f5b1f5b8d5eb13b6be
MichaelPapinutto <michael.papinutto@unifr.ch>
2018-05-11 18:30:52 +0200

    "test from file + bug connection 5000 always up"

--- test/bash_test.sh
+++ test/bash_test.sh
@@ -4,10 +4,16 @@ cd $(dirname $0)
 pwd
 cd ..
 cd ./cmake-build-debug/src/client
-
+rm list-cmd.txt
+for i in {1..500}
+do
+    echo "delete $i">> list-cmd.txt
+    echo "add $i $(((RANDOM % 100) + 1 ))">> list-cmd.txt
+done
+echo "ls" >> list-cmd.txt
+echo "exit" >> list-cmd.txt
 start=$(date +'%s%3N')
 ./client 127.0.0.1  < list-cmd.txt
-
 end=$(date +'%s%3N')
 runtime=$((end-start))
 echo $runtime

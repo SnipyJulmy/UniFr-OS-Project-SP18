bacade211d415e41478d012621a4bb8ae80fd4a6
Sylvain Julmy <sylvain.julmy@gmail.com>
2018-05-24 13:53:06 +0200

    modify indexes in the script

--- benchmarks/no_collisions/create_script.sh
+++ benchmarks/no_collisions/create_script.sh
@@ -3,29 +3,29 @@
 rm -f *.log
 rm -f *.cmd
 
-for i in {1..10}
+for i in {1..8}
 do
     FILE="client_$i.cmd"
     touch ${FILE}
 
     # add 1000 element in the format $i_$j and value $j
-    for j in {0..10}
+    for j in {100..110}
     do
-        KEY="$i$i$j"
+        KEY="$i$j"
         VALUE="$j"
         echo "add $KEY $VALUE" >> ${FILE}
     done
 
-    for j in {0..10}
+    for j in {100..110}
     do
-        KEY="$i$i$j"
+        KEY="$i$j"
         VALUE="$j"
         echo "read_v $KEY" >> ${FILE}
     done
 
-    for j in {0..10}
+    for j in {100..110}
     do
-        KEY="$i$i$j"
+        KEY="$i$j"
         VALUE="$j"
         echo "rm_k $KEY" >> ${FILE}
     done
@@ -41,11 +41,13 @@ fi
 
 touch ${RUN_SCRIPT}
 echo '#!/usr/bin/env bash' >> ${RUN_SCRIPT}
-for i in {1..10}
+for i in {1..4}
 do
     FILE="client_$i.cmd"
     LOG="client_$i.log"
-    echo "../../cmake-build-debug/src/client/client -f ./$FILE -l ./$LOG 127.0.0.1 &" >> ${RUN_SCRIPT}
+    LOG_PROC="client_${i}_proc.log"
+    LOG_ERR="client_${i}_err.log"
+    echo "../../cmake-build-debug/src/client/client -f ./$FILE -l ./$LOG 127.0.0.1 >$LOG_PROC 2> $LOG_ERR &" >> ${RUN_SCRIPT}
 done
 
 chmod +x ${RUN_SCRIPT}
